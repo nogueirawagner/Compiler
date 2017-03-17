1c95d762451c64c3e34ea603009055ab89682d19
Wagner Nogueira <wagner@ephrom.com.br>
2017-03-17 17:56:41 -0300

    Ajuste em validação de token

--- Compiller/main.c
+++ Compiller/main.c
@@ -5,7 +5,7 @@ int main(int argc, char** argv) {
 
 	source_t* source = ts_open_source("C:\\Users\\ephrom\\Documents\\Visual Studio 2015\\Projects\\Compiller\\Debug\\Source.chs");
 
-	token_type_t types[5] = { TK_TYPE, TK_ID, TK_EQUAL, TK_CONSTINT, TK_STM_END };
+	token_type_t types[5] = { TK_TYPE, TK_ID, TK_EQUAL, TK_CONST, TK_STM_END };
 	int i = 0;
 	while (1) 
 	{
